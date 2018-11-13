914c539e11bffbadda5d3079169d1b69c50070a3
Alexey <grimzest@gmail.com>
2018-11-13 12:07:06 +0300

    remove console.log from gtag

--- src/utils/gtag.js
+++ src/utils/gtag.js
@@ -1,7 +1,6 @@
 export default (type, options) => {
 	const { gtag } = window;
 	if(typeof gtag !== 'undefined'){
-		console.log('gtag', type, options);
 		gtag('event', type, options);
 	}else{
 		console.log('Gtag is not defined');
